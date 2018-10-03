fe2e38788887f2aa2712822308a6f87292da3d6f
hunter <hsuchyj@udel.edu>
2018-10-02 21:10:27 -0400

    yes

--- sh.c
+++ sh.c
@@ -29,10 +29,23 @@ int sh( int argc, char **argv, char **envp )
   char cwd[256];
   pid_t pid;
   int n;
-  char** argArr = malloc(sizeof(char**));//*******************************************************************NEED TO CHANGE TO **
+  //char* argArr1[];
+
   int histLength = 25;
-  char** history[25];
+  char** history = malloc(100 * sizeof(char*));//*******************************************************************NEED TO CHANGE TO **
+  for (i = 0; i < histLength; ++i)
+  {
+    history[i] = (char *)malloc(sizeof(char*));
+  }
+  for (i = 0; i < histLength; ++i)
+  {
+    history[i] = '\0';
+  }
+  char* prefix = malloc(sizeof(char*));
   int histIndex = 0;
+  int numCommands = 0;
+
+  prefix = "";
   uid = getuid();
   password_entry = getpwuid(uid);               /* get passwd info */
   homedir = password_entry->pw_dir;		/* Home directory to start
@@ -56,50 +69,66 @@ int sh( int argc, char **argv, char **envp )
   signal(SIGINT, catchCtrlC);
   //sigignore(SIGSTOP);
   //sigignore(SIGTERM);
+  char* line = calloc(MAX_CANON, sizeof(char));
+  char* cmd = malloc(128);
+  //char*cwd = malloc(sizeof(char*));
   while( go )
   {
-    char* line = calloc(MAX_CANON, sizeof(char));
-    char* cmd = malloc(128);
-    char*cwd = malloc(128);
+    char** argArr = malloc(100 * sizeof(char*));//*******************************************************************NEED TO CHANGE TO **
+    for (i = 0; i < 10; ++i)
+    {
+      argArr[i] = (char *)malloc(sizeof(char*));
+    }
+    for (i = 0; i < 10; ++i)
+    {
+      argArr[i] = '\0';
+    }
+    argArr[9] = NULL;
+    //prefix = "";
     /* print your prompt */
     //printf("%s",getcwd(cwd, PATH_MAX+1));
     getcwd(cwd, PATH_MAX+1);
-    sprintf(line,"%s[%s]>",prompt,cwd);
+    sprintf(line,"%s %s[%s]>",prefix,prompt,cwd);
     printf("%s",line);
 
     /* get command line and process */
     fgets(commandline, 200, stdin);
-    char* token = strtok(commandline, " ");
+
+    //adds space to end of command line
+    commandline[strlen(commandline) - 1] = ' ';
+    commandline[strlen(commandline)] = '\0';
     int k = 0;
     int len;
+    char * token = strtok(commandline, " ");
     while(token != NULL)
     {
-      argArr[k] = token;
+      //printf("%s\n", argArr[k]);
+      argArr[k++] = token;
       token = strtok(NULL, " ");
-      k++;
+      //token = strtok(NULL, " ");
     }
     len = k;
+    printf("this is len %i\n", len);
     command = argArr[0];
-    if(histIndex == histLength)
+    if(command != NULL)
     {
-      histIndex = 0;
-    }
-    else
-    {
-      history[histIndex] = command;
-      histIndex++;
-    }
+    //printf("this is before %s ind %i\n", history[0], histIndex);
+    history[histIndex] = &command;
+    //printf("this is after %s\n", history[0]);
+    //printf("this is hist %s\n", history[histIndex]);
+    numCommands++;
 
-    printf("this is argument 0 %s 1 %s len %lu\n", argArr[0], argArr[1], strlen(argArr[1]));
+    //printf("this is argument 0 %s 1 %s len %lu\n", argArr[0], argArr[1], strlen(argArr[1]));
     //command[strlen(command)-1] = '\0';
     //checking for wildcard *
     int wildcard = 0;
     char* wild;
+    int checkBuilt = 0;
     //k is length of array or num of args
     //printf("this is size %lu\n", (sizeof(argArr) / sizeof(argArr[0])));
     for(int i = 0; i < k; i++)
     {
-      printf("this is the stringer %s\n",argArr[i]);
+      //printf("this is the stringer %s\n",argArr[i]);
       for(int j = 0; j < sizeof(argArr[i])/sizeof(argArr[i][0]); j++)
       {
           if(argArr[i][j] == '*')
@@ -116,7 +145,7 @@ int sh( int argc, char **argv, char **envp )
       glob_t  paths;
       int     csource;
       char    **p;
-      wild[strlen(wild)-1] = '\0';
+      //wild[strlen(wild)-1] = '\0';
       csource = glob(wild, 0, NULL, &paths);
 
       if (csource == 0)
@@ -128,6 +157,7 @@ int sh( int argc, char **argv, char **envp )
         }
 
         globfree(&paths);
+        checkBuilt = 1;
       }
     }
 
@@ -139,16 +169,83 @@ int sh( int argc, char **argv, char **envp )
       if(strcmp(builtins[i],command) == 0)
       {
           built = builtins[i];
+          checkBuilt = 1;
           printf("Executing built in %s\n", built);
       }
+    }
+    //absolute paths
+    if(checkBuilt == 0)
+    {
+      char* whichExe = malloc(sizeof(char*));
+      char first = argArr[0][0];
+      //argArr[0][strlen(argArr[0])-1] = '\0';
+      //printf("%lu\n", strlen(argArr[1]));
+      //if(first == '/' || first == '.')
+      //{
+        //printf("this is access %i\n", access(argArr[0],F_OK));
+        //printf("this is len %lu\n", strlen(argArr[0]));
+        //printf("%s\n",strerror(errno));
+
+        if(first == '/' || first == '.')
+        {
+          //execve(argArr[0], argArr,  envp);
+          whichExe = argArr[0];
+        }
+        else
+        {
+          if(which(command, pathlist)!= NULL)
+          {
+            printf("which is working %s\n",which(command, pathlist));
+            //execve(which(command, pathlist), argArr,  envp);
+            //for ls
+            if(access(which(command, pathlist),F_OK) == 0)
+            {
+              whichExe = which(command, pathlist);
+            }
+            else
+            {
+              printf("does not exit\n");
+            }
+          }
+        }
+        //printf("this is exe  %s\n", whichExe);
+        //if(access(which(command, pathlist),F_OK) == 0)
+        //{
+          //printf("this is exe %i\n", execve(argArr[0],argv, envp));
+          pid = fork();
+          //printf("l l %lu\n",strlen(argArr[1]));
+          if(pid ==-1)
+          {
+            perror("fork error");
+          }
+          else if(pid > 0)
+          {
+            int status;
+            printf("this is parent this is pid %i\n", pid);
+            waitpid(pid,&status, 0);
+          }
+          else if (pid == 0)
+          {
+            printf("this is child this is pid %i\n", pid);
+            //absolute path or ls
+            execve(whichExe, argArr,  envp);
+          }
+            //printf("Return not expected. Must be an execve error.n\n");
+          //execve(argArr[0],argv, envp);
+        //}
+      //}
+      printf("%s\n",strerror(errno));
+      free(whichExe);
     }
     //printf("%lu\n", strlen(built));
     if(strcmp(built,"cd") == 0)
     {
       //printf("build is cd\n");
-      char first = argArr[1][0];
-      argArr[1][strlen(argArr[1])-1] = '\0';
+      //argArr[1][strlen(argArr[1])-1] = '\0';
       //printf("%lu\n", strlen(argArr[1]));
+      if(argArr[1] != NULL)
+      {
+        char first = argArr[1][0];
       if(first == '/')
       {
         prev = getcwd(prev, PATH_MAX+1);
@@ -157,6 +254,7 @@ int sh( int argc, char **argv, char **envp )
       else if(strcmp(argArr[1],"-") == 0)
       {
         chdir(prev);
+      }
       }
       else
       {
@@ -170,7 +268,32 @@ int sh( int argc, char **argv, char **envp )
     }
     else if(strcmp(built,"history")==0)
     {
+      int num = 10;
+      if(numCommands < 10)
+      {
+        num = numCommands;
+        printf("shrink num %i\n", num);
+      }
+      if(argArr[1] != NULL)
+      {
+        if(isdigit(atoi(argArr[1])) > 0)
+        {
+          num = atoi(argArr[1]);
+        }
+      }
+      int i = 0;
+      int tempInd = histIndex;
+      for(int i = 0; i < len; i++)
+      {
+        printf("name uno %s\n", history[i]);
+      }
+      while(i < num)
+      {
+        printf("name %s\n", history[tempInd]);
+        tempInd--;
+        i++;
 
+      }
     }
     else if(strcmp(built,"list") == 0)
     {
@@ -183,7 +306,7 @@ int sh( int argc, char **argv, char **envp )
         int i = 1;
         while(argArr[i] != NULL)
         {
-          argArr[i][strlen(argArr[i])-1] = '\0';
+          //argArr[i][strlen(argArr[i])-1] = '\0';
           d = opendir(argArr[i]);
           printf("\n");
           printf("%s:\n", argArr[i]);
@@ -208,6 +331,10 @@ int sh( int argc, char **argv, char **envp )
           free(d);
       }
     }
+    else if (strcmp(built,"pid") == 0)
+    {
+      printf("%i\n",getpid());
+    }
     else if (strcmp(built,"printenv") == 0)
     {
       if(argArr[2] != NULL)
@@ -228,6 +355,47 @@ int sh( int argc, char **argv, char **envp )
         //printf("%s\n",getenv(argArr[1]));
       }
     }
+    else if (strcmp(built,"prompt") == 0)
+    {
+      if(isspace(argArr[1][0]) > 0)
+      {
+        printf("input prompt prefix: ");
+        fgets(prefix, 200, stdin);
+        prefix[strlen(prefix)-1] = '\0';
+      }
+      else
+      {
+        prefix = argArr[1];
+        prefix[strlen(prefix)-1] = '\0';
+      }
+    }
+    else if (strcmp(built,"pwd") == 0)
+    {
+      printf("%s\n",cwd);
+    }
+    else if(strcmp(built,"where") == 0)
+    {
+      where(command,pathlist);
+    }
+    else if (strcmp(built,"which") == 0)
+    {
+      //printf("%s\n" , argArr[2]);
+      for(int i = 1; i < len; i++)
+      {
+        printf("suze iz %i\n", len);
+        if(argArr[i] != NULL)
+        {
+          printf("dis length %lu\n", strlen(argArr[i]));
+          //argArr[i][strlen(argArr[i]) - 1] = '\0';
+          printf("jes %s\n",argArr[i]);
+          char* args = malloc(500);
+          args = which(argArr[i],pathlist);
+          printf("%s\n", args);
+          free(args);
+        }
+      }
+    }
+    histIndex++;
     /*
     else if (strcmp(built,"setenv") == 0)
     {
@@ -278,6 +446,14 @@ int sh( int argc, char **argv, char **envp )
       }
       printf("%s\n",strerror(errno));
 
+      //free(cmd);
+      //free(cwd);
+      //free(line);
+      //free(argArr);
+      for (i = 0; i < 10; ++i)
+      {
+        free(argArr[i]);
+      }
     }
     /*
     if(which(command, pathlist)!= NULL)
@@ -297,7 +473,8 @@ int sh( int argc, char **argv, char **envp )
   //}
   //free(cmd);
   //free(cwd);
-  free(line);
+  //free(line);
+    }
   }
   return 0;
 } /* sh() */
@@ -312,8 +489,8 @@ char *which(char *command, struct pathelement *pathlist )
 {
    /* loop through pathlist until finding command and return it.  Return
    NULL when not found. */
-   char* cmd = malloc(sizeof(char*));
-   char* commandVal = NULL;
+   char* cmd = malloc(360);
+   char* commandVal = malloc(360);
    //printf("%s", pathlist->element);
    while (pathlist)
    {
@@ -321,11 +498,11 @@ char *which(char *command, struct pathelement *pathlist )
      strcat(cmd, "/");
      //command[strlen(command)-1] = '\0';
      strcat(cmd, command);
-     //printf("%s", cmd);
+     //printf("%s\n", cmd);
      //printf("%i", access("/usr/bin/cd.sh", F_OK));
-     cmd[strlen(cmd)-1] = '\0';
+     //cmd[strlen(cmd)-1] = '\0';
      //printf("\n");
-     //printf("%lu", strlen(cmd));
+     //printf("%lu\n", strlen(cmd));
      //printf("\n");
      //printf("%lu", strlen("/usr/bin/cd.sh"));
      //printf("\n");
@@ -339,13 +516,14 @@ char *which(char *command, struct pathelement *pathlist )
      */
      if(access(cmd, F_OK) == 0)
      {
-        //printf("%i",access(cmd, F_OK));
-        commandVal = command;
+        printf("%i\n",access(cmd, F_OK));
+        commandVal = cmd;
+        break;
       }
      pathlist = pathlist->next;
    }
 
-   return commandVal;
+   //return commandVal;
    /*
    char* commandVal = NULL;
    struct pathelement* temp = pathlist->next;
@@ -365,7 +543,9 @@ char *which(char *command, struct pathelement *pathlist )
    }
    return commandVal;
    */
-
+   return commandVal;
+   //free(cmd);
+   //free(commandVal);
 } /* which() */
 
 
@@ -382,7 +562,7 @@ void *where(char *command, struct pathelement *pathlist )
     cmd[strlen(cmd)-1] = '\0';
     if(access(cmd, F_OK) == 0)
     {
-       printf("%s\n"cmd);
+       printf("%s\n",cmd);
     }
     pathlist = pathlist->next;
   }
